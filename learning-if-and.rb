06b1106852859323fdcb7319a291fa1e91af40e8
Iuliia Saprykina <iuliia.proskurnina@gmail.com>
2021-01-27 17:23:24 +0000

    Automatically backed up by Learn

---
+++ learning-if-and.rb
@@ -0,0 +1,24 @@
+# run_code_inside = false
+# puts "Code before if...end"
+# if run_code_inside
+#   puts "code inside"
+# end
+# puts "Code after if...end"
+
+
+# chance_of_rain = 1
+# puts "Let's go outside!"
+# if chance_of_rain > 0.5
+#   puts "Pack an umberella!"
+# else
+#   puts "Enjoy the fine day!"
+# end
+# puts "Oh, and always wear sunscreen!"
+
+greeting = "friendly greeting"
+case greeting
+ when "unfriendly greeting"
+   puts "What do you want?"
+when "friendly greeting"
+   puts "Hi! How are you?"
+end
